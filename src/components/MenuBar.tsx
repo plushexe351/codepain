8f5c98bdef6e006ce82429fc1cb0c729a19fb2a6
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2025-09-08 07:25:51 +0530

    update index.html, add favicon and logo

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import ExportButton from "./ExportButton";
+import logo from "../assets/logo.png";
 
 interface Props {
   html: string;
@@ -11,6 +12,7 @@ const MenuBar: React.FC<Props> = ({ html, css, js }) => {
   return (
     <div className="menubar">
       <h1 className="logo">
+        <img src={logo} alt="" className="logo-img" />
         Code<span>pain</span>
       </h1>
       {(html || css || js) && <ExportButton html={html} css={css} js={js} />}
