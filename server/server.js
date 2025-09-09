ff0b452e03510c37ab94e6fb91757aebc1544b6c
Kalpan Talukder <kalpantalukder10@gmail.com>
2025-09-10 03:34:22 +0530

    env file update

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000" || "http://127.0.0.1:5173",
+    origin: process.env.CLIENT_URL || "http://127.0.0.1:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
