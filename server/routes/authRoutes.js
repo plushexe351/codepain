e4f3feeccb74a0be226f096ddbc0c89f934bc94a
Kalpan Talukder <kalpantalukder10@gmail.com>
2025-09-11 15:19:33 +0530

    chatgpt had one job

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,7 @@ import {
   getUserByUsername,
   userLogout,
 } from "../controllers/authController.js";
-import { protect } from "../middleware/authHandler.js";
+import { protect } from "../middlewares/authHandler.js";
 
 const authRoutes = express.Router();
 
